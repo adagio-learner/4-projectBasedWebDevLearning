56d0dbe6d2330bdd9576326343edf66a2db389ab
adagio_learner <social.ayan.khan@gmail.com>
2025-06-29 23:52:08 +0530

    add showWinner and checkWinner function

--- 1 TicTacToe/script.js
+++ 1 TicTacToe/script.js
@@ -64,3 +64,26 @@ const disabledBoxes = () => {
     }
 }
 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation, Winner is ${winner}🥳`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+}
+
+const checkWinner = () => {
+    for(pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                showWinner(pos1Val);
+                return true;
+            }
+        }
+    }
+}
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
