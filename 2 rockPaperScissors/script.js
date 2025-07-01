959d1596b014ddd0c2643d19ea340184ce5f6cfb
adagio_learner <social.ayan.khan@gmail.com>
2025-07-01 17:11:52 +0530

    create function playGame, genCompChoice

--- 2 rockPaperScissors/script.js
+++ 2 rockPaperScissors/script.js
@@ -7,6 +7,37 @@ const msgParagraph = document.querySelector("#msg");
 const userChoicePara = document.querySelector("#user-score");
 const compChoicePara = document.querySelector("#comp-score");
 
+const genCompChoice = () => {
+    const option = ["rock", "paper", "scissors"];
+    const randIndex = Math.floor(Math.random() * 3);
+    return option[randIndex];
+}
+
+const drawGame = () => {
+    msgParagraph.innerText = "Game was Draw. Play again";
+    msgParagraph.style.backgroundColor = "grey";
+}
+
+const playGame = (userChoice) => {
+    // Generate Computer Choice
+    const compChoice = genCompChoice();
+
+    if(userChoice === compChoice){
+        // Draw Game
+        drawGame();
+    } else {
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        } else if(userChoice === "paper"){
+            userWin = compChoice === "scissors" ? false : true;
+        }else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+}
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
