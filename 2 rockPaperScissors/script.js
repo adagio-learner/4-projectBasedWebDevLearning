88f53621cc1f727fe2f38b9c4ba7d70a472c95f8
adagio_learner <social.ayan.khan@gmail.com>
2025-07-01 17:10:52 +0530

    select element using querySelector

--- 2 rockPaperScissors/script.js
+++ 2 rockPaperScissors/script.js
@@ -1,6 +1,12 @@
 let userScore = 0;
 let compScore = 0;
 
+const choices = document.querySelectorAll(".choice");
+const msgParagraph = document.querySelector("#msg");
+
+const userChoicePara = document.querySelector("#user-score");
+const compChoicePara = document.querySelector("#comp-score");
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.getAttribute("id");
